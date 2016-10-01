17193e15aaa579c4c11112d88d580f19ce6bd35c
Chris Phillips <chrisjp88@gmail.com>
2016-10-01 02:29:29 +0100

    initial release

---
+++ CJPAdMobHelper/CJPAdMobHelper.m
@@ -0,0 +1,407 @@
+//
+//  CJPAdMobHelper.m
+//  CJPAdMobHelper
+//
+//  Created by Chris Phillips on 30/09/2016.
+//  Copyright © 2016 Midnight Labs Ltd. All rights reserved.
+//
+
+#import "CJPAdMobHelper.h"
+
+// Debug logging
+#if defined(DEBUG) && defined(DEBUG_CJPADMOBHELPER)
+#define CJPLog(fmt, ...) NSLog((@"%@ [line %u]: " fmt), NSStringFromClass(self.class), __LINE__, ##__VA_ARGS__)
+#else
+#define CJPLog(...)
+#endif
+
+static NSString * const CJPAdsPurchasedKey = @"adRemovalPurchased";
+
+@interface CJPAdMobHelper ()
+
+@property (nonatomic, strong) GADBannerView *adMobView;
+@property (nonatomic, strong) UIViewController *contentController;
+@property (nonatomic, strong) UIView *containerView;
+@property (nonatomic, assign) BOOL showingAd;
+@property (nonatomic, strong) NSDictionary *adMobUserLocation;
+
+- (void)createBanner;
+- (void)removeBannerAndPermanently:(BOOL)permanent;
+- (void)layoutAds;
+- (UIViewController *)currentViewController;
+
+@end
+
+@implementation CJPAdMobHelper
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (CJPAdMobHelper *)sharedInstance
+{
+    static CJPAdMobHelper *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    
+    return sharedInstance;
+}
+
+- (id)init
+{
+    if ((self = [super init]))
+    {
+        // Have ads been removed?
+        _adsRemoved = [[NSUserDefaults standardUserDefaults] boolForKey:CJPAdsPurchasedKey];
+        
+        // Set defaults
+        _initialDelay = 0.0;
+        _useAdMobSmartSize = YES;
+    }
+    return self;
+}
+
+- (void)startWithViewController:(UIViewController *)contentController
+{
+    _contentController = contentController;
+    
+    // Create a container view to hold our parent view and the banner view
+    _containerView = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self addChildViewController:_contentController];
+    [_containerView addSubview:_contentController.view];
+    [_contentController didMoveToParentViewController:self];
+    
+    // Set the container view as this view
+    self.view = _containerView;
+    
+    // Now everything is set up, we can create a banner (if the user hasn't purchased ad removal)
+    if (!_adsRemoved) {
+        [self performSelector:@selector(createBanner) withObject:nil afterDelay:_initialDelay];
+    }
+}
+
+#pragma mark -
+#pragma mark Banner Create/Destroy
+
+- (void)createBanner
+{
+    BOOL isPortrait = UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]);
+    BOOL isIPad = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? YES : NO;
+    
+    // Create AdMob banner
+    CJPLog(@"Creating AdMob");
+    GADAdSize adMobSize;
+    if (_useAdMobSmartSize) {
+        adMobSize = isPortrait ? kGADAdSizeSmartBannerPortrait : kGADAdSizeSmartBannerLandscape;
+        _adMobView = [[GADBannerView alloc] initWithAdSize:adMobSize];
+    }
+    else {
+        // Legacy AdMob ad sizes don't fill the full width of the device screen apart from iPhone when in portrait view
+        // We need to offset the x position so the ad appears centered - Calculation: (View width - Ad width) / 2
+        // Problem is that getting the width of the bounds doesn't take into account the current orientation
+        // As a workaround, if we're in landscape, we'll simply get the height instead
+        CGRect screen = [[UIScreen mainScreen] bounds];
+        CGFloat screenWidth = isPortrait ? CGRectGetWidth(screen) : CGRectGetHeight(screen);
+        adMobSize = isIPad ? kGADAdSizeLeaderboard : kGADAdSizeBanner;
+        CGSize cgAdMobSize = CGSizeFromGADAdSize(adMobSize);
+        CGFloat adMobXOffset = (screenWidth-cgAdMobSize.width)/2;
+        _adMobView = [[GADBannerView alloc] initWithFrame:CGRectMake(adMobXOffset, self.view.frame.size.height - cgAdMobSize.height, cgAdMobSize.width, cgAdMobSize.height)];
+    }
+    
+    // Specify the ad's "unit identifier." This is your AdMob Publisher ID.
+    _adMobView.adUnitID = _adMobUnitID;
+    
+    // Set initial frame to be off screen
+    CGRect bannerFrame = _adMobView.frame;
+    bannerFrame.origin.y = [[UIScreen mainScreen] bounds].size.height;
+    _adMobView.frame = bannerFrame;
+    
+    // Let the runtime know which UIViewController to restore after taking
+    // the user wherever the ad goes and add it to the view hierarchy.
+    _adMobView.rootViewController = self;
+    _adMobView.delegate = self;
+    _adMobView.hidden = YES;
+    [_containerView insertSubview:_adMobView atIndex:0];
+    
+    // Request an ad
+    GADRequest *adMobRequest = [GADRequest request];
+    
+    // Device identifier strings that will receive test AdMob ads
+    if (_testDeviceIDs!=nil) {
+        adMobRequest.testDevices = _testDeviceIDs;
+    }
+    
+    /*
+     COPPA
+     
+     If this app has been tagged as being aimed at children (1), or not for children (0), send the value with the ad request
+     Ignore if the tag has not been set
+     */
+    //
+    if ([_tagForChildDirectedTreatment isEqualToString:@"0"] || [_tagForChildDirectedTreatment isEqualToString:@"1"]) {
+        BOOL tagForCOPPA = [_tagForChildDirectedTreatment isEqualToString:@"1"] ? YES : NO;
+        [adMobRequest tagForChildDirectedTreatment:tagForCOPPA];
+        CJPLog(@"COPPA has been set to %i", tagForCOPPA);
+    }
+    
+    /*
+     Targeting
+     
+     We only send this information with our request if they have been explicitly set
+     */
+    
+    // Gender
+    if (_adMobGender != kGADGenderUnknown) {
+        adMobRequest.gender = _adMobGender;
+        CJPLog(@"AdMob targeting: Gender has been set to %i", (int)_adMobGender);
+    }
+    
+    // Birthday
+    if (_adMobBirthday != nil) {
+        adMobRequest.birthday = _adMobBirthday;
+        CJPLog(@"AdMob targeting: Birthday has been set.");
+    }
+    
+    // Location
+    if (_adMobUserLocation != nil) {
+        [adMobRequest setLocationWithLatitude:[[_adMobUserLocation objectForKey:@"latitude"] floatValue] longitude:[[_adMobUserLocation objectForKey:@"longitude"] floatValue] accuracy:[[_adMobUserLocation objectForKey:@"accuracy"] floatValue]];
+        CJPLog(@"AdMob targeting: Location has been set to %@", _adMobUserLocation);
+    }
+    
+    // Now we can load the requested ad
+    [_adMobView loadRequest:adMobRequest];
+    CJPLog(@"Added AdMob to view and requested ad.");
+}
+
+- (void)removeBannerAndPermanently:(BOOL)permanent
+{
+    // When `permanently` is NO
+    // This method simply hides the banner from view - the banner will show again when the next ad request is fired...
+    // This will be 2 minutes for AdMob (but can be changed in your AdMob account)
+    
+    // When `permanently` is YES
+    // This method will set the banner's view to nil and remove the banner completely from the container view
+    // A new banner will not be shown unless you call restartAds.
+    
+    _showingAd = NO;
+    CGRect bannerFrame = _adMobView.frame;
+    bannerFrame.origin.y = [[UIScreen mainScreen] bounds].size.height;
+    _adMobView.frame = bannerFrame;
+    [_containerView sendSubviewToBack:_adMobView];
+    if (permanent) {
+        _adMobView.delegate = nil;
+        [_adMobView removeFromSuperview];
+        _adMobView = nil;
+        CJPLog(@"Permanently removed AdMob from view.");
+    }
+    else {
+        CJPLog(@"Temporarily hiding AdMob off screen.");
+    }
+
+    [UIView animateWithDuration:0.25 animations:^{
+        [self layoutAds];
+    }];
+}
+
+#pragma mark Remove ads from view
+
+- (void)removeAdsAndMakePermanent:(BOOL)permanent andRemember:(BOOL)remember
+{
+    // Make sure any performSelector requests are canceled...
+    // Fixes a bug where if a long initial delay is given, a user could remove ads after the perform request is sent but
+    // before the delay ends, leading to ads created when they shouldn't be
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+    
+    // Remove ad banner from the view
+    if (_adMobView!=nil) [self removeBannerAndPermanently:permanent];
+    
+    // Set adsRemoved to YES, and store in NSUserDefaults for future reference if remember and permanent both true
+    if (permanent && remember) {
+        _adsRemoved = YES;
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:CJPAdsPurchasedKey];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+}
+
+- (void)removeAds
+{
+    [self removeAdsAndMakePermanent:NO andRemember:NO];
+}
+
+#pragma mark Restart Ad Serving
+
+- (void)restartAdsAfterDelay:(NSTimeInterval)delay
+{
+    // This method restores ads to the view by creating a new banner, intended to be used after removeBanner permanently is called.
+    // NOTE: The boolean _adsRemoved is taken into account.
+    
+    if (!_adsRemoved && !_showingAd) {
+        CJPLog(@"Restarting ad serving...");
+        [self performSelector:@selector(createBanner) withObject:nil afterDelay:delay];
+    }
+}
+
+- (void)restartAds
+{
+    [self restartAdsAfterDelay:0.0];
+}
+
+
+#pragma mark -
+#pragma mark View Methods
+
+- (void)layoutAds
+{
+    [self.view setNeedsLayout];
+}
+
+// Returns the currently visible view controller from either the UINavigationController or UITabBarController holding the content
+- (UIViewController *)currentViewController
+{
+    return (UIViewController*)[_contentController.childViewControllers lastObject];
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    // Return the application's statusBarHidden if the UIViewControllerBasedStatusBarAppearance key has not been added to Info.plist
+    // Otherwise return the prefersStatusBarHidden set by the view controller
+    if (![[NSBundle mainBundle] objectForInfoDictionaryKey:@"UIViewControllerBasedStatusBarAppearance"]) {
+        return [UIApplication sharedApplication].statusBarHidden;
+    }
+    else {
+        return [self currentViewController].prefersStatusBarHidden;
+    }
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    // Return the application's statusBarStyle if the UIViewControllerBasedStatusBarAppearance key has not been added to Info.plist
+    // Otherwise return the preferredStatusBarStyle set by the view controller
+    if (![[NSBundle mainBundle] objectForInfoDictionaryKey:@"UIViewControllerBasedStatusBarAppearance"]) {
+        return [UIApplication sharedApplication].statusBarStyle;
+    }
+    else {
+        return [self currentViewController].preferredStatusBarStyle;
+    }
+}
+
+- (BOOL)shouldAutorotate
+{
+    return [[self currentViewController] shouldAutorotate];
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return [self currentViewController].supportedInterfaceOrientations;
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return [self currentViewController].preferredInterfaceOrientationForPresentation;
+}
+
+- (void)viewDidLayoutSubviews
+{
+    BOOL isPortrait = UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]);
+    BOOL isPad = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? YES : NO;
+    CGRect contentFrame = self.view.bounds;
+    CGRect bannerFrame = CGRectZero;
+    
+    // If an AdMob view has been created we'll figure out which views need adjusting
+    if (_useAdMobSmartSize) {
+        _adMobView.adSize = isPortrait ? kGADAdSizeSmartBannerPortrait : kGADAdSizeSmartBannerLandscape;
+        bannerFrame = _adMobView.frame;
+    }
+    else {
+        // Legacy AdMob doesn't have different orientation sizes - we just need to change the X offset so the ad remains centered
+        bannerFrame = _adMobView.frame;
+        CGRect screen = [[UIScreen mainScreen] bounds];
+        CGFloat screenWidth = isPortrait ? CGRectGetWidth(screen) : CGRectGetHeight(screen);
+        GADAdSize adMobSize = isPad ? kGADAdSizeLeaderboard : kGADAdSizeBanner;
+        CGSize cgAdMobSize = CGSizeFromGADAdSize(adMobSize);
+        CGFloat adMobXOffset = (screenWidth-cgAdMobSize.width)/2;
+        bannerFrame.origin.x = adMobXOffset;
+        _adMobView.frame = bannerFrame;
+    }
+    
+    // Now if we actually have an ad to display
+    if (_showingAd) {
+        CJPLog(@"AdView exists and ad is being shown.");
+        
+        contentFrame.size.height -= bannerFrame.size.height;
+        bannerFrame.origin.y = contentFrame.size.height;
+    }
+    // Or if we don't...
+    else {
+        CJPLog(@"AdView exists but there is currently no ad to be shown. Waiting for new ad...");
+        bannerFrame.origin.y = contentFrame.size.height;
+    }
+    
+    if (_showingAd) _adMobView.frame = bannerFrame;
+    
+    _contentController.view.frame = contentFrame;
+}
+
+#pragma mark - AdMob Delegate Methods
+
+- (void)adViewDidReceiveAd:(GADBannerView *)bannerView
+{
+    CJPLog(@"New AdMob ad received.");
+    
+    _showingAd = YES;
+    _adMobView.hidden = NO;
+    
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         [self layoutAds];
+                     }
+                     completion:^(BOOL finished){
+                         // Ensure view isn't behind the container and untappable
+                         if (finished) [_containerView bringSubviewToFront:_adMobView];
+                     }];
+}
+
+- (void)adView:(GADBannerView *)view didFailToReceiveAdWithError:(GADRequestError *)error
+{
+    // Ensure view is hidden off screen
+    if (_adMobView.frame.origin.y>=0 && _adMobView.frame.origin.y < _containerView.frame.size.height) {
+        [self removeBannerAndPermanently:NO];
+    }
+    _showingAd = NO;
+    
+    CJPLog(@"Failed to receive AdMob. %@", error.localizedDescription);
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self layoutAds];
+    }];
+}
+
+// Unused AdMob delegate methods
+//- (void)adViewWillPresentScreen:(GADBannerView *)bannerView
+//{
+//
+//}
+
+//- (void)adViewDidDismissScreen:(GADBannerView *)bannerView
+//{
+//
+//}
+
+//- (void)adViewWillLeaveApplication:(GADBannerView *)bannerView
+//{
+//
+//}
+
+#pragma mark - AdMob Targeting
+
+- (void)setLocationWithLatitude:(CGFloat)latitude longitude:(CGFloat)longitude accuracy:(CGFloat)accuracyInMeters
+{
+    _adMobUserLocation = @{
+                           @"latitude" : [NSNumber numberWithFloat:latitude],
+                           @"longitude" : [NSNumber numberWithFloat:longitude],
+                           @"accuracy" : [NSNumber numberWithFloat:accuracyInMeters]
+                           };
+}
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# CJPAdMobHelper
-Singleton class enabling easy implementation of AdMob banner ads in UINavigationController based iOS apps.
+# CJPAdMobHelper 1.0
+
+CJPAdMobHelper is a singleton class enabling easy implementation of Google AdMob banner ads in your iOS app.
+
+There are some assumptions/limitations regarding intended usage. They include:
+* This class is designed to be used in a `UINavigationController` showing a banner at the bottom of every view. Other classes may work but are neither supported nor tested.
+* AdMob is your primary ad network.
+* This class does not handle any mediation itself, it is assumed you will (optionally) use AdMob's front-end for mediation with other ad networks.
+* Showing interstitial/videos or any other types of adverts is not currently supported but may be added in the future if time permits (feel free to submit a pull request if you have time to add this yourself).
+
+## Features
+* No need to add any views or constraints in your existing storyboards.
+* Specify a time delay for when to start showing ads after the app is launched
+* Works for both iPhone and iPad in any orientation (rotation is handled automatically) (iOS 8.x and later).
+* Hides from view if there are no ads to display
+* If you have an IAP allowing your users to disable ads there is method you can call to permanently hide ads for them
+
+
+## Screenshots
+
+[![CJPAdMobHelper screenshot](http://i.imgur.com/daylYpcm.png)](http://i.imgur.com/daylYpc.png) [![CJPAdMobHelper screenshot](http://i.imgur.com/RwaWJXhm.png)](http://i.imgur.com/RwaWJXh.png)
+
+
+## Adding to your project
+
+### Method 1 - CocoaPods
+
+[CocoaPods](http://cocoapods.org) is the simplest way to get started. Just add `pod 'CJPAdMobHelper'` to your Podfile and run `pod install`.
+
+### Method 2 - Old School
+
+Drop the `CJPAdMobHelper` folder into your project and download and install the latest [**AdMob SDK**](https://firebase.google.com/docs/admob/ios/download), following any instructions in the readme.
+
+## Usage
+
+CJPAdMobHelper will automatically display ads at the top or bottom of your view. It is designed to be used with a UINavigationController.
+
+**1.** `#import "CJPAdMobHelper.h"` in your `AppDelegate.m`.
+
+**2.** In `application didFinishLaunchingWithOptions`, configure CJPAdMobHelper to your liking using the sharedInstance, you will at the very least need to provide the unit ID for the AdMob banner you want to display:
+
+```objective-c
+[CJPAdMobHelper sharedInstance].adMobUnitID = @"ca-app-pub-1234567890987654/1234567890";
+```
+
+**3.** Set up your navigation controller as usual, then tell CJPAdMobHelper to start serving ads in it.
+
+```objective-c
+[[CJPAdMobHelper sharedInstance] startWithViewController:_yourNavController];
+```
+
+**4.** One more thing... you'll need to set the window's rootViewController to the sharedInstance of CJPAdMobHelper
+
+```objective-c
+self.window.rootViewController = [CJPAdMobHelper sharedInstance];
+```
+
+## Configuration Options
+
+### Delay the appearance of ads after app has launched
+By default, ads will be requested as soon as your app is launched. You can delay this by providing an NSTimeInterval, the following code would wait 5 seconds after your app has launched before requesting an ad:
+
+```objective-c
+[CJPAdMobHelper sharedInstance].initialDelay = 5.0;
+```
+
+## Targeting
+There are a number of specific options that can also be configured, as well as a number of general methods for hiding, removing, restoring ads etc.
+AdMob ads may also be targeted based on your users' age, gender and location. Please read the comments in the header file before using any of these.
+You can see an example of these in the demo project, furthermore, the header file is well commented with information on what each method does and how you might want to use them both in testing or in production.
+
+
+## Licence and Attribution
+If you're feeling kind you can provide attribution and a link to this GitHub project.
+
+
+### Licence
+Copyright (c) 2016 Chris Phillips
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
